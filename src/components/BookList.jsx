335330e8b1919a60941c7eccf851f1de05dc0bf9
mshr1-creat <19950786m@gmail.com>
2024-09-16 11:26:51 +0900

    コメントの追加

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import '../pages/bookReviews.scss'; // BEMのクラス設計に基づいたCSS
 import PropTypes from 'prop-types';
 
+// books配列をmap関数で反復し、各書籍の情報をリストアイテムとして表示する
 const BookList = ({ books }) => {
   return (
     <ul className="book-reviews__list">
@@ -19,7 +20,7 @@ const BookList = ({ books }) => {
   );
 };
 
-// PropTypesを追加
+// PropTypesを追加 プロパティの型チェックを行うため。
 BookList.propTypes = {
   books: PropTypes.array.isRequired,
 };
